7742d0c9e171c4618458aaaf3a9d2ccad4503d3b
SIRxRIS <faizraziq1@gmail.com>
2025-07-16 08:24:35 +0800

    update

---
+++ src/firebase-comment.js
@@ -0,0 +1,23 @@
+// src/firebase-comment.js
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { collection, addDoc } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize with a unique name
+const app = initializeApp(firebaseConfig, "comments-app");
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { db, storage, collection, addDoc };
